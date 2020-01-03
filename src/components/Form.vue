d2e0f34da3e4ffc7def814454e22026e2f612f9d
Thanh Pham Kieu <thanhpk@live.com>
2020-01-03 17:45:28 +0700

    fix form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,4 @@
 <script>
-	import PrimaryButton from './PrimaryButton.vue'
 	export default {
 		name:'form',
 		props:{form: Object},
@@ -15,8 +14,6 @@
 				<textarea  v-if="field.multiline_text" class='textarea' :placeholder="field.placeholder" maxlength="1000"/>
 				<input v-else  class="input" id="name" :placeholder="field.placeholder"/><br>
 			</div>
-			<PrimaryButton :primarybutton="form.primary_button" type="submit" />
 		</form>
 	</div>
 </template>
-
--- src/modal/template2.vue
+++ src/modal/template2.vue
@@ -1,135 +1,139 @@
 <template>
-   <div class="container">
-				<div class="template-header">
-					<button class="header-button-close">
-						</button>
-				</div>
-				<div class="template-main">
-					<div class="text-container">
-						<p class="template-main--title">{{this.page.title}}</p>
-						<p class="template-main--description">{{this.page.description}}</p>
-					</div>
-					<div class="template-main--inputs-container">
-						<Form :form="page" class="template-main--input"/>
-					</div>
+	<div class="container">
+		<div class="template-header">
+			<button class="header-button-close">
+			</button>
+		</div>
+		<div class="template-main">
+			<div class="text-container">
+				<p class="template-main--title">{{this.page.title}}</p>
+				<p class="template-main--description">{{this.page.description}}</p>
+			</div>
+			<div class="template-main--inputs-container">
+				<Form :form="page" class="template-main--input"/>
+				<div class="buttons-container">
+					<button class="primary-button">
+						<p class="primary-button--text">{{this.page.primary_button.text}}</p>
+					</button>
 				</div>
 			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import Form from '../components/Form.vue'
 export default {
-		name: 'template2',
-		props:{page: Object},
-		components: {
-    Form
-  },
+	name: 'template2',
+	props:{page: Object},
+	components: {Form},
 }
 </script>
+
 <style scoped>
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
-	.container{
-			display: flex;
-			flex-direction: column;
-			height: 520px;
-			width: 800px;
-			align-items: center;
-			background-image: url('../assets/bg2.png');
-			background-color: #d26;
-			background-size:auto;
-	}
-	.template-header{
-		display: flex;
-		align-self: flex-end;
-		padding: 3px;
-	}
-	.header-button-close{
-		display: flex;
-		height: 20px;
-		width: 20px;
-		border-radius: 50%;
-		border:0px;
-	}
-	.template-main {
-		display:  flex;
-		flex-direction: column;
-		margin-top: 10px;
-		padding-top:10px;
-		height:450px;
-		width: 430px;
-		align-items: center;
-		background-color: #fff;
-	}
-	.text-container{
-		margin-left: 50px;
-		margin-right: 50px;
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-end;
-		flex:1;
-	}
-	.template-main--title {
-		font-size: 30px;
-		font-weight: bold;
-		font-family:'Montserrat', sans-serif;
-		font-style: normal;
-		text-align: center;
-		width: 50;
-	}
-	.template-main--description {
-		margin-top:10px;
-		display: flex;
-		font-family: 'Muli';
-		font-weight: normal;
-		text-align: center;
-		font-size: 18px;
-	}
-	.template-main--inputs-container{
-		display: flex;
-		flex-direction:column;
-		flex:2;
-		width: 75%;
-		align-items: center;
-		justify-content:center;
-	}
-	.template-main--input{
-		margin-top:15px;
-		width: 100% ;
-		height: 40px;
-		border-top-width: 0px;
-		border-right-width: 0px;
-		border-left-width: 0px;
-		border-bottom-color: #cccccc;
-		border-bottom-width: 1px;
-	}
-	.template-main--buttons-container{
-		display: flex;
-		margin-top: 5px;
-		align-items: center;
-		justify-content: center;
-	}
-	.template-main--primary-button{
-			margin-top:20px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width:200px;
-		height: 45px;
-		border:0px;
-		border-radius: 5px;
-		background-color: #FDAD15;
-	}
-	.primary-button--text{
-		color: #fff;
-		font-size: 16px;
-		line-height: 19px;
-		font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-		font-weight: 500;
-		text-align: center;
-		text-transform: uppercase;
-	}
+ * {
+	 margin: 0;
+	 padding: 0;
+	 box-sizing: border-box;
+ }
+ .container{
+	 display: flex;
+	 flex-direction: column;
+	 height: 520px;
+	 width: 800px;
+	 align-items: center;
+	 background-image: url('../assets/bg2.png');
+	 background-color: #d26;
+	 background-size:auto;
+ }
+ .template-header{
+	 display: flex;
+	 align-self: flex-end;
+	 padding: 3px;
+ }
+ .header-button-close{
+	 display: flex;
+	 height: 20px;
+	 width: 20px;
+	 border-radius: 50%;
+	 border:0px;
+ }
+ .template-main {
+	 display:  flex;
+	 flex-direction: column;
+	 margin-top: 10px;
+	 padding-top:10px;
+	 height:450px;
+	 width: 430px;
+	 align-items: center;
+	 background-color: #fff;
+ }
+ .text-container{
+	 margin-left: 50px;
+	 margin-right: 50px;
+	 display: flex;
+	 flex-direction: column;
+	 justify-content: flex-end;
+	 flex:1;
+ }
+ .template-main--title {
+	 font-size: 30px;
+	 font-weight: bold;
+	 font-family:'Montserrat', sans-serif;
+	 font-style: normal;
+	 text-align: center;
+	 width: 50;
+ }
+ .template-main--description {
+	 margin-top:10px;
+	 display: flex;
+	 font-family: 'Muli';
+	 font-weight: normal;
+	 text-align: center;
+	 font-size: 18px;
+ }
+ .template-main--inputs-container{
+	 display: flex;
+	 flex-direction:column;
+	 flex:2;
+	 width: 75%;
+	 align-items: center;
+	 justify-content:center;
+ }
+ .template-main--input{
+	 margin-top:15px;
+	 width: 100% ;
+	 height: 40px;
+	 border-top-width: 0px;
+	 border-right-width: 0px;
+	 border-left-width: 0px;
+	 border-bottom-color: #cccccc;
+	 border-bottom-width: 1px;
+ }
+ .template-main--buttons-container{
+	 display: flex;
+	 margin-top: 5px;
+	 align-items: center;
+	 justify-content: center;
+ }
+ .template-main--primary-button{
+	 margin-top:20px;
+	 display: flex;
+	 align-items: center;
+	 justify-content: center;
+	 width:200px;
+	 height: 45px;
+	 border:0px;
+	 border-radius: 5px;
+	 background-color: #FDAD15;
+ }
+ .primary-button--text{
+	 color: #fff;
+	 font-size: 16px;
+	 line-height: 19px;
+	 font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	 font-weight: 500;
+	 text-align: center;
+	 text-transform: uppercase;
+ }
 </style>
