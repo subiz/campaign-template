aa59636793902459e80d9a66009cb387c2e38ffb
lilq <dquanktpm@gmail.com>
2020-01-07 09:56:06 +0700

    add fullscreen campaign

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+import Template1 from "./src/modal/template1.vue";
 import Template2 from "./src/modal/template2.vue";
 import Template3 from "./src/modal/template3.vue";
 import Template4 from "./src/modal/template4.vue";
-import Template5 from "./src/modal/template5.vue";
+import Template5 from "./src/fullscreen/template5.vue";
+import Template6 from "./src/fullscreen/template6.vue";
+
+import Template9 from "./src/modal/template9.vue";
+import Template10 from "./src/modal/template10.vue";
+
 
 export default {
-  Template2,
-  Template3,
-  Template4,
-  Template5
+  modal:{
+    Template1,
+    Template2,
+    Template3,
+    Template4,
+    Template9,
+    Template10
+  },
+  fullscreen:{
+    Template5,
+    Template6,
+  }
 };
