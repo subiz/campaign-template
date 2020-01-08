d4e183b5842ab852d19636a32e9f663d644b7c2e
Thanh Pham Kieu <thanhpk@live.com>
2020-01-08 12:28:33 +0700

    remove unused templates

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
 import template1 from './src/modal/template1.vue'
-import template1_2 from './src/modal/template1_2.vue'
-
 import template2 from './src/modal/template2.vue'
-import template2_2 from './src/modal/template2_2.vue'
-
 import template3 from './src/modal/template3.vue'
-import template3_2 from './src/modal/template3_2.vue'
-
 import template4 from './src/modal/template4.vue'
-import template4_2 from './src/modal/template4_2.vue'
-
 import template5 from './src/fullscreen/template5.vue'
-import template5_2 from './src/fullscreen/template5_2.vue'
-
 import template6 from './src/fullscreen/template6.vue'
-import template6_2 from './src/fullscreen/template6_2.vue'
-
 import template9 from './src/modal/template9.vue'
-import template9_2 from './src/modal/template9_2.vue'
-
 import template10 from './src/modal/template10.vue'
-import template10_2 from './src/modal/template10_2.vue'
 
 export default {
 	modal: {
 		template1,
-		template1_2,
-
 		template2,
-		template2_2,
-
 		template3,
-		template3_2,
-
 		template4,
-		template4_2,
-
 		template9,
-		template9_2,
-
 		template10,
-		template10_2,
 	},
 	fullscreen: {
 		template5,
-		template5_2,
 
 		template6,
-		template6_2,
 	},
 }
