6544f9ac17570614af65a8b764ff1909a7b8c3ae
Thanh Pham Kieu <thanhpk@live.com>
2020-01-08 20:01:33 +0700

    build css for template 1_2, 2 and 2_2

--- src/modal/template2.vue
+++ src/modal/template2.vue
@@ -7,7 +7,7 @@ export default {
 	name: 'template2',
 	props:["page", 'mode'],
 	created(){
-		if (this.mode === 'mobile') this.mobile = true 
+		if (this.mode === 'mobile') this.mobile = true
 	},
 	components: {Form},
 	data(){
@@ -65,7 +65,7 @@ export default {
 			<div class="buttons-container">
 				<button @click="onPrimaryClick"  v-show="op(this.page,'primary_button.enabled',true)"  class="primary-button">
 					{{op(this.page,"primary_button.text", this.primary_button_text)}}
-					
+
 				</button>
 				<button @click="onSecondaryClick" v-show="op(this.page,'secondary_button.enabled',true)" class="secondary-button">
 					{{op(this.page, "secondary_button.text", this.secondary_button_text)}}
@@ -75,168 +75,4 @@ export default {
 	</div>
 </template>
 
-<style scoped>
-*{
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
-.container {
-	display: flex;
-	flex-direction: column;
-	width: 800px;
-	max-width: 100%;
-	align-items: center;
-	background-image: url('../assets/bg2.png');
-	background-size: auto;
-	margin: 0 auto;
-	position: relative;
-}
-
-
-.button-close {
-	position: absolute !important;
-	right: 10px !important;
-	top: 10px !important;
-	height: 20px !important;
-	width: 20px !important;
-	border-radius: 50% !important;
-	border: 0px !important;
-	background-image: url('../assets/close.png') !important;
-	background-size:100% 100% !important;
-}
-
-.main {
-	display: flex !important;
-	flex-direction: column !important;
-	padding-top:45px !important;
-	width: 430px !important;
-	max-width: 100% !important;
-	align-items: center !important;
-	background-color: #fff !important;
-	margin-top: 36px !important;
-	margin-bottom: 36px !important;
-	box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.0196802), 0px 2px 4px rgba(0, 0, 0, 0.03), 0px 7px 12px rgba(0, 0, 0, 0.0503198), 0px 18px 28px rgba(0, 0, 0, 0.07) !important;
-}
-
-.title {
-	font-size: 30px !important;
-	font-weight: bold !important;
-	font-style: normal !important;
-	text-align: center !important;
-	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif !important;
-	margin-left: 50px;
-	margin-right: 50px;
-}
-
-.description {
-	margin-top: 10px !important;
-	font-weight: normal !important;
-	text-align: center !important;
-	font-size: 18px !important;
-	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif !important;
-	margin-left: 27px;
-	margin-right: 27px;
-}
-
-.buttons-container{
-	display: flex !important;
-	margin-top: 30px !important;
-	margin-bottom:  30px !important;
-	align-items: center !important;
-	justify-content: center !important;
-}
-
-.primary-button {
-	border:0px !important;
-	border-radius: 5px !important;
-	background-color: #FDAD15 ;
-	color: #fff !important;
-	font-size: 16px !important;
-	font-weight: 500 !important;
-	text-align: center !important;
-	text-transform: uppercase !important;
-	padding-left: 30px !important;
-	padding-right: 30px !important;
-	padding-top: 12px !important;
-	padding-bottom: 12px !important;
-	cursor: pointer !important;
-	outline: 0 !important;
-}
-
-.secondary-button {
-	border:0px  !important;
-	border-radius: 5px !important;
-	background-color: #d1d1d1 !important; 
-	color: #fff !important;
-	font-size: 16px !important;
-	font-weight: 500 !important;
-	text-align: center !important;
-	text-transform: uppercase !important;
-	padding-left: 30px !important;
-	padding-right: 30px !important;
-	padding-top: 12px !important;
-	padding-bottom: 12px !important;
-	margin-left: 10px !important;
-	cursor: pointer !important;
-	outline: 0 !important;
-}
-
- .secondary-button:hover {
-	 color: #bbbbbb !important;
- }
- .primary-button:hover{
-	 background-color: #de9813 !important;
- }
-/deep/ .form {
-	display: flex !important;
-	flex-direction: column !important;
-	width: 100% !important;
-	margin-top: 15px !important;
-	align-items: center;
-}
-/deep/ .form-item{
-	display: flex !important;
-	width: 80% !important;
-	flex-direction: column !important;
-}
-/deep/ .label{
-	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-	font-size: 10px;
-	color: #000;
-	margin-top:10px;
-}
-/deep/ .text-input {
-border: none !important;
-border-bottom: 1px solid #d9d9d9 !important; 
-width: 100% !important;
-height: 40px !important;
-font-size: 16px !important;
-margin-top: 5px !important;
-padding-left: 5px !important;
-outline: 0 !important;
-}
-.container.mobile{
-	padding-top: 20px !important;
-	padding-left: 25px !important;
-	padding-right: 25px !important;
-	width: 95% !important;
-}
-
-.mobile .title{
-	margin-left:10px !important;
-	margin-right:10px !important;
-}
-
-.mobile .buttons-container {
-	padding-left: 10px !important;
-	padding-right: 10px !important;
-	width: 100% !important;
-}
-.mobile .buttons-container .primary-button{
-	font-size: 12px !important;
-}
-.mobile .buttons-container .secondary-button{
-	font-size: 12px !important;
-}
-</style>
+<style src="./template2.css"></style>
