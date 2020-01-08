111513dc48e0561caddf25ffd0806634f6c55d8a
Thanh Pham Kieu <thanhpk@live.com>
2020-01-08 12:22:48 +0700

    fix index

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-import Template1 from "./src/modal/template1.vue";
-import Template2 from "./src/modal/template2.vue";
-import Template3 from "./src/modal/template3.vue";
-import Template4 from "./src/modal/template4.vue";
-import Template5 from "./src/fullscreen/template5.vue";
-import Template6 from "./src/fullscreen/template6.vue";
+import template1 from './src/modal/template1.vue'
+import template1_2 from './src/modal/template1_2.vue'
 
-import Template9 from "./src/modal/template9.vue";
-import Template10 from "./src/modal/template10.vue";
+import template2 from './src/modal/template2.vue'
+import template2_2 from './src/modal/template2_2.vue'
 
+import template3 from './src/modal/template3.vue'
+import template3_2 from './src/modal/template3_2.vue'
 
-import Template1_2 from "./src/modal/template1_2.vue";
-import Template2_2 from "./src/modal/template2_2.vue";
-import Template3_2 from "./src/modal/template3_2.vue";
-import Template4_2 from "./src/modal/template4_2.vue";
-import Template5_2 from "./src/fullscreen/template5_2.vue";
-import Template6_2 from "./src/fullscreen/template6_2.vue";
+import template4 from './src/modal/template4.vue'
+import template4_2 from './src/modal/template4_2.vue'
 
-import Template9_2 from "./src/modal/template9_2.vue";
-import Template10_2 from "./src/modal/template10_2.vue";
+import template5 from './src/fullscreen/template5.vue'
+import template5_2 from './src/fullscreen/template5_2.vue'
 
+import template6 from './src/fullscreen/template6.vue'
+import template6_2 from './src/fullscreen/template6_2.vue'
+
+import template9 from './src/modal/template9.vue'
+import template9_2 from './src/modal/template9_2.vue'
+
+import template10 from './src/modal/template10.vue'
+import template10_2 from './src/modal/template10_2.vue'
 
 export default {
-  modal:{
-    Template1,
-    Template2,
-    Template3,
-    Template4,
-    Template9,
-    Template10,
-    Template1_2,
-    Template2_2,
-    Template3_2,
-    Template4_2,
-    Template9_2,
-    Template10_2
-  },
-  fullscreen:{
-    Template5,
-    Template6,
-    Template5_2,
-    Template6_2,
-  }
-};
+	modal: {
+		template1,
+		template1_2,
+
+		template2,
+		template2_2,
+
+		template3,
+		template3_2,
+
+		template4,
+		template4_2,
+
+		template9,
+		template9_2,
+
+		template10,
+		template10_2,
+	},
+	fullscreen: {
+		template5,
+		template5_2,
+
+		template6,
+		template6_2,
+	},
+}
