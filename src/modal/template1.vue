51e25b6e5fb9a3cd6a89e8b4c868b468b634ecbd
Thanh Pham Kieu <thanhpk@live.com>
2020-01-09 10:18:44 +0700

    clean templates

--- src/modal/template1.vue
+++ src/modal/template1.vue
@@ -1,82 +1,27 @@
-<script>
- var common = require('../common.js')
- var op = require('object-path')
- import Form from '../components/Form.vue'
- export default {
-	 name: 'template1',
-	 props: ['page','mode'],
-	 components:{Form},
-	 created(){
-		 if (this.mode === 'mobile') this.mobile = true
-	 },
-	 data(){
-		 return{
-			 mobile:  common.mobilecheck(),
-			 close:false,
-			 id:"template1",
-			 name:"Get leads",
-			 title:"Giảm 10%",
-			 description:"với đơn hàng đầu tiên của bạn",
-			 primary_button_text:"Đăng ký ngay",
-			 secondary_button_text:"Không, cảm ơn. Tôi sẽ quay lại sau",
-			 form:{
-				 fields:[
-					 {
-						 key :1,
-						 label : "Ho Ten",
-						 is_required :true,
-						 type :"text",
-						 placeholder:"Họ tên"
-					 },
-					 {
-						 key :2,
-						 label : "Email cua ban",
-						 is_required :true,
-						 type :"text",
-						 placeholder:"Email của bạn"
-					 },
-				 ],
-			 },
-			 close: false,
-		 }
-	 },
-	methods:{
-		op: op.get,
-		onClose(){
-			this.$emit("closeButtonClicked")
-			this.close = true
-		},
-		onSecondaryClick(){
-			this.$emit("secondaryButtonClicked")
-		},
-		onPrimaryClick(){
-			this.$emit("primaryButtonClicked")
-		}
-	}
- }
-</script>
+<script src="base.js"></script>
+<style src="./template1.css"></style>
+
 <template>
-<div class="template template1">
-	<div v-if="!close" :class="'container ' + (mobile ? 'mobile': '')">
-		<button class="button-close" @click="onClose"></button>
-		<div class="main">
-			<div class="image"></div>
-			<div class="div-left"></div>
-			<div class="div-right">
-				<p class="title">{{op(this.page, "title",this.title)}}</p>
-				<p class="description">{{op(this.page, "description",this.description)}}</p>
-				<Form :form="op(this.page,'form',this.form)" />
-				<div class="buttons-container">
-					<button @click="onPrimaryClick" v-show="op(this.page,'primary_button.enabled',true)" class="primary-button">
-						{{op(this.page,"primary_button.text", this.primary_button_text)}}
-					</button>
-					<a @click="onSecondaryClick" v-show="op(this.page,'secondary_button.enabled',true)" class="secondary-button">
-						{{op(this.page, "secondary_button.text", this.secondary_button_text)}}
-					</a>
+	<div class="template template1">
+		<div v-if="!close" :class="'container ' + mode">
+			<button class="button-close" @click="onClose"></button>
+			<div class="main">
+				<div class="image"></div>
+				<div class="div-left"></div>
+				<div class="div-right">
+					<p class="title">{{page.title}}</p>
+					<p class="description">{{page.description}}</p>
+					<Form :form="page.form" />
+					<div class="buttons-container">
+						<button @click="onPrimaryClick" v-show="showPrimaryButton" class="primary-button">
+							{{primaryButtonText}}
+						</button>
+						<a @click="onSecondaryClick" v-show="showSecondaryButton" class="secondary-button">
+							{{secondaryButtonText}}
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	</div>
 </template>
-<style src="./template1.css"></style>
--- src/modal/template2.vue
+++ src/modal/template2.vue
@@ -1,80 +1,23 @@
+<style src="./template2.css"></style>
+<script src="./base.js"></script>
 
-<script>
-import Form from '../components/Form.vue'
-var op = require('object-path')
-var common = require('../common.js')
-export default {
-	name: 'template2',
-	props:["page", 'mode'],
-	created(){
-		if (this.mode === 'mobile') this.mobile = true
-	},
-	components: {Form},
-	data(){
-		return{
-			mobile: common.mobilecheck(),
-			close: false,
-			id:"template2",
-			name:"Get leads",
-			title:"Đăng ký để nhận ưu đãi đặc biệt",
-			description:"Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá.",
-			primary_button_text:"Đăng ký ngay",
-			form:{
-				fields:[
-            {
-              key :1,
-              label : "Ho Ten",
-              is_required :true,
-              type :"text",
-              placeholder:"Họ tên"
-            },
-            {
-              key :2,
-              label : "Email cua ban",
-              is_required :true,
-              type :"text",
-              placeholder:"Email của bạn"
-            },
-          ],
-			},
-			secondary_button_text:"Cancel",
-		}
-	},
-	methods:{
-		op: op.get,
-		onClose(){
-			this.$emit("closeButtonClicked")
-			this.close = true;
-		},
-		onSecondaryClick(){
-			this.$emit("secondaryButtonClicked")
-		},
-		onPrimaryClick(){
-			this.$emit("primaryButtonClicked")
-		}
-	}
-}
-</script>
 <template>
-<div class="template template2">
-	<div v-if="!close" :class="'container '+(mobile ? 'mobile': '')  ">
-		<button class="button-close" @click="onClose"></button>
-		<div class="main">
-			<p class="title">{{op(this.page, "title",this.title)}}</p>
-			<p class="description">{{op(this.page, "description",this.description)}}</p>
-			<Form :form="op(this.page,'form',this.form)" />
-			<div class="buttons-container">
-				<button @click="onPrimaryClick"  v-show="op(this.page,'primary_button.enabled',true)"  class="primary-button">
-					{{op(this.page,"primary_button.text", this.primary_button_text)}}
-
-				</button>
-				<button @click="onSecondaryClick" v-show="op(this.page,'secondary_button.enabled',true)" class="secondary-button">
-					{{op(this.page, "secondary_button.text", this.secondary_button_text)}}
-				</button>
+	<div class="template template2">
+		<div v-if="!close" :class="'container '+ mode ">
+			<button class="button-close" @click="onClose"></button>
+			<div class="main">
+				<p class="title">{{page.title}}</p>
+				<p class="description">{{page.description)}}</p>
+				<Form :form="page.form" />
+				<div class="buttons-container">
+					<button @click="onPrimaryClick"  v-show="showPrimaryButton" class="primary-button">
+						{{primaryButtonText}}
+					</button>
+					<button @click="onSecondaryClick" v-show="showSecondaryButton" class="secondary-button">
+						{{secondaryButtonText}}
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
-	</div>
 </template>
-
-<style src="./template2.css"></style>
