6b74df8a9e5c3fb3253c24d9e00c05e2b741a662
lilq <dquanktpm@gmail.com>
2020-01-10 18:20:24 +0700

    update App.js

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template class="tem">
   <div id="subiz" class="app"  >
-    <Template mode="maobile" :page="page"/>
+    <Template mode="mobisle" :page="page"/>
   </div>
 </template>
 <script>
-import Template from './modal/template1.vue'
+import Template from './modal/template17.vue'
 export default {
   name: 'app',
   components: {
@@ -18,10 +18,10 @@ export default {
         "description": "Chúng tôi đã nhận được yêu cầu liên hệ của bạn và chúng tôi sẽ gọi lại cho bạn vào thời gian đã chọn",
         primary_button:{
           enabled : true,
-          text: "ĐĂNG KÝ NGAY"
+          text: "Đóng"
         },
 				secondary_button:{
-          enabled : true,
+          enabled : false,
           text: "Cancel"
         },
       },
@@ -65,26 +65,26 @@ export default {
         },
       },
       templatehavesubtitle:{
-        title:"NHẬN NGAY ƯU ĐÃI AAAAA",
+        title:"NHẬN NGAY ƯU ĐÃI",
         subtitle:"Đăng ký",
         form_enabled:true,
         images:[],
         form:{
            enabled:true,
           fields:[
+              {
+            key :1,
+            label : "Họ tên",
+            is_required :true,
+            type :"text",
+            placeholder:"Họ tên",
+          },
           {
             key :2,
             label : "Email",
             is_required :true,
             type :"text",
-            placeholder:"Email cua ban",
-          },
-          {
-            key :3,
-            label : "So dien thoai ",
-            is_required :true,
-            type :"text",
-            placeholder:"So dien thoai cua ban",
+            placeholder:"Email của bạn",
           }
         ],
         },
@@ -95,55 +95,57 @@ export default {
           redirect_url : 11,
           open_url : 12
         },
+         secondary_button:{
+          text:"Đóng",
+          enabled : false,
+          action : 10,
+          redirect_url : 11,
+          open_url : 12
+        },
         description:"Giảm 20% chỉ một ngày duy nhất. Miễn phí vận chuyển nội thành.",
         primaryButtonTitle:"Đăng ký nhanh"
       },
       templatefeedback:{
         title:"Oh không! Tại sao bạn lại rời khỏi đây quá sớm như thế?",
-        subtitle:"",
-        form_enabled:true,
-        images:[],
+        description:"Phản hồi của bạn rất quan trọng với chúng tôi.",
+
         form:{
           enabled:true,
-           fields:[
-          {
-            key :1,
-            label : "Cảm nhận của bạn",
-            is_required :true,
-            type :"text",
-            multiline_text:true,
-            placeholder:"Bạn đang nghĩ gì ...",
-          },
-          {
-            key :2,
-            label : "Email",
-            is_required :true,
-            type :"text",
-            multiline_text:false,
-            placeholder:"johndoe@example.com",
-          },
-        ],
-
+          fields:[
+            {
+              key :1,
+              label : "Cảm nhận của bạn",
+              is_required :true,
+              type :"text",
+              multiline_text:true,
+              placeholder:"Bạn đang nghĩ gì ...",
+            },
+            {
+              key :2,
+              label : "Email",
+              is_required :true,
+              type :"text",
+              multiline_text:false,
+              placeholder:"johndoe@example.com",
+            },
+          ],
         },
         primary_button:{
-        text:"Gửi cảm nhận",
-        enabled : true,
+          text:"Gửi cảm nhận",
+          enabled : true,
         },
-         secondary_button:{
+        secondary_button:{
           text:"Cancle",
           enabled : false,
 
         },
-        description:"Phản hồi của bạn rất quan trọng với chúng tôi.",
-        primaryButtonTitle:"Đăng ký nhanh"
-        },
-       templatefeedback2:{
+      },
+      templatefeedback2:{
         title:"Bạn có tìm được thứ mình cần?",
-        subtitle:"",
-        form_enabled:true,
-        images:[],
-        fields:[
-
+        description:"Chia sẻ cảm nhận của bạn với chúng tôi và bạn sẽ nhận được 10% GIẢM GIÁ.",
+        form:{
+          enable :true,
+          fields:[
           {
             key :1,
             label : "Cảm nhận của bạn",
@@ -159,33 +161,30 @@ export default {
             type :"email",
             multiline_text:false,
             placeholder:"Email của bạn",
+          },
+          ],
         },
-        ],
         primary_button:{
           text:"Gửi cảm nhận",
           enabled : true,
-          action : 10,
-          redirect_url : 11,
-          open_url : 12
         },
-        description:"Chia sẻ cảm nhận của bạn với chúng tôi và bạn sẽ nhận được 10% GIẢM GIÁ.",
-        primaryButtonTitle:"Đăng ký nhanh"
+        secondary_button:{
+          text:"Đóng",
+          enabled : false,
+        },
       },
     }
   }
 }
 </script>
 <style >
-* {
-  margin: 0;
-  padding:0;
+#subiz * {
+  all:unset;
 }
 .app{
   display: flex;
-  align-items: center;
-  justify-content: center;
   height: 100vh;
   width: 100%;
-  background-color: bisque;
+  background-color: #0f1a2d;
 }
 </style>
