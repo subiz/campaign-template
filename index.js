603a68d309946521738f966ddafacecfaeb2cdc4
Thanh Pham Kieu <thanhpk@live.com>
2020-01-17 11:25:25 +0700

    publish

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import template1 from './src/modal/template1/template1.vue'
-import template2 from './src/modal/template2/template2.vue'
-import template3 from './src/modal/template3/template3.vue'
-import template4 from './src/modal/template4/template4.vue'
-import template5 from './src/fullscreen/template5/template5.vue'
-import template6 from './src/fullscreen/template6/template6.vue'
-import template7 from './src/modal/template7/template7.vue'
-import template9 from './src/modal/template9/template9.vue'
-import template10 from './src/modal/template10/template10.vue'
-import template17 from './src/modal/template17/template17.vue'
-import templateu23 from './src/modal/templateu23/templateu23.vue'
+let template1 = import (/*webpackChunkName: "template1" */' ./src/modal/template1/template1.vue')
+let template2 = import (/*webpackChunkName: "template2" */' ./src/modal/template2/template2.vue')
+let template3 = import (/*webpackChunkName: "template3" */' ./src/modal/template3/template3.vue')
+let template4 = import (/*webpackChunkName: "template4" */' ./src/modal/template4/template4.vue')
+let template5 = import (/*webpackChunkName: "template5" */' ./src/modal/template5/template5.vue')
+let template6 = import (/*webpackChunkName: "template6" */' ./src/modal/template6/template6.vue')
+let template9 = import (/*webpackChunkName: "template9" */' ./src/modal/template9/template9.vue')
+let template10 = import (/*webpackChunkName: "template10" */' ./src/modal/template10/template10.vue')
+let template17 = import (/*webpackChunkName: "template17" */' ./src/modal/template17/template17.vue')
+let templateu23 = import (/*webpackChunkName: "templateu23" */' ./src/modal/templateu23/templateu23.vue')
 
 export default {
 	modal: {
