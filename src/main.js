170ce7e11e93dda9817ecdb810ddd3be5ee94d78
Thanh Pham Kieu <thanhpk@live.com>
2020-01-19 22:31:33 +0700

    fix app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,48 @@
 import Vue from 'vue'
-import App from './App.vue'
-
 Vue.config.productionTip = false
 
+import INDEX from '../index.js'
+let { Template, index } = INDEX
+
+var common = require('./common.js')
+
 new Vue({
-  render: h => h(App),
+	name: 'App',
+	data () {
+		return {
+			template: 'template1', // CHANGE THIS TO SWITCH TEMPLATE
+			page: {
+				title: '',
+				description: '',
+				form: {
+					enabled: true,
+					fields: [
+						{ key: 1, label: 'Họ Tên', is_required: true, type: 'text', placeholder: 'John Doe' },
+						{ key: 2, label: 'Email', is_required: true, type: 'text', placeholder: 'johndoe@example.com' },
+						{ key: 3, label: 'Họ Tên', is_required: true, type: 'text', placeholder: 'John Doe' },
+						{ key: 4, label: 'Emails', is_required: true, type: 'text', placeholder: 'johndoe@example.com' },
+					],
+				},
+				primary_button: { enabled: true, text: '' },
+				secondary_button: { enabled: true, text: '' },
+			},
+		}
+	},
+	mounted () {
+		var metadata = index[this.template]
+		this.page.title = metadata.title.vi
+		this.page.description = metadata.description.vi
+		this.page.primary_button.text = metadata.primary_button_text.vi
+		this.page.secondary_button.text = metadata.secondary_button_text.vi
+
+		// make the environment look like widget environment
+		common.setCssToHead('subiz-template-style-app', '#subiz * {all:unset;}')
+	},
+	render (h) {
+		return (
+			<div id="subiz">
+				<Template template={this.template} page={this.page} />
+			</div>
+		)
+	},
 }).$mount('#app')
