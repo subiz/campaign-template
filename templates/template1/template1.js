919d88c0bb3caa1951bcb28ae7b5a0d0a01577da
Thanh Pham Kieu <thanhpk@live.com>
2020-01-19 22:27:30 +0700

    relocate templates

---
+++ templates/template1/template1.js
@@ -0,0 +1,24 @@
+export default {
+	name: 'template1',
+	props: ['page', 'form', 'countdown', 'primaryButton', 'secondaryButton', 'closeButton'],
+	render (h) {
+		return (
+			<div class="container">
+				{this.closeButton}
+				<div class="main-template">
+					<div class="image"></div>
+					<div class="div-left"></div>
+					<div class="div-right">
+						<p class="title">{this.page.title}</p>
+						<p class="description">{this.page.description}</p>
+						{this.form}
+						<div class="buttons-container">
+							{this.primaryButton}
+							{this.secondaryButton}
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	},
+}
