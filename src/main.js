270f30f836a5337cda09d8a7efd31e8ba68f6f59
lilq <dquanktpm@gmail.com>
2020-02-13 13:41:35 +0700

    template3 , fix path_name

--- src/main.js
+++ src/main.js
@@ -11,33 +11,34 @@ new Vue({
 		return {
 			template: 'template1', // CHANGE THIS TO SWITCH TEMPLATE
 			page: {
-				title: 'Giam 10%',//'Đăng ký để nhận ưu đãi đặc biệt',
-				description: 'với đơn hàng đầu tiên của bạn',// 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
+				title: '',//'Đăng ký để nhận ưu đãi đặc biệt',
+				description: '',// 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
 				form: {
 					enabled: true,
 					fields: [
 						{ key: 1, label: 'Họ Tên', is_required: true, type: 'text', placeholder: 'John Doe' },
 						{ key: 2, label: 'Email', is_required: true, type: 'text', placeholder: 'johndoe@example.com' },
 						{ key: 3, label: 'Họ Tên', is_required: true, type: 'text', placeholder: 'John Doe' },
-						{ key: 4, label: 'Emails', is_required: true, type: 'text', placeholder: 'johndoe@example.com' },
-						{ key: 2, label: 'Email', is_required: true, type: 'text', placeholder: 'johndoe@example.com' },
+						// { key: 1, label: 'Họ Tên', is_required: true, type: 'text', placeholder: 'John Doe' },
+							// { key: 2, label: 'Email', is_required: true, type: 'text', placeholder: 'johndoe@example.com' },
+							
 					],
 				},
-				primary_button: { enabled: true, text: 'DANG KY NGAY ' },
-				secondary_button: { enabled: true, text: 'Khong, cam on toi se quay lai sau' },
+				primary_button: { enabled: true, text: '' },
+				secondary_button: { enabled: true, text: '' },
 			},
 		}
 	},
 	mounted () {
 		this.template = window.location.pathname.substr(1)
-		var metadata = Template.meta[this.template].en
-		if (!op.get(this.page, 'title')) op.set(this.page, 'title', op.get(metadata, 'title'))
-		if (!op.get(this.page, 'description')) op.set(this.page, 'description', op.get(metadata, 'description'))
+		var metadata = Template.meta[this.template].text
+		if (!op.get(this.page, 'title')) op.set(this.page, 'title', op.get(metadata, 'title.vi'))
+		if (!op.get(this.page, 'description')) op.set(this.page, 'description', op.get(metadata, 'description.vi'))
 		if (!op.get(this.page, 'primary_button.text')) {
-			op.set(this.page, 'primary_button.text', op.get(metadata, 'primary_button.text'))
+			op.set(this.page, 'primary_button.text', op.get(metadata, 'primary_button_text.vi'))
 		}
 		if (!op.get(this.page, 'secondary_button.text')) {
-			op.set(this.page, 'secondary_button.text', op.get(metadata, 'secondary_button.text'))
+			op.set(this.page, 'secondary_button.text', op.get(metadata, 'secondary_button_text.vi'))
 		}
 		// make the environment look like widget environment
 		// common.setCssToHead('subiz-template-style-app', '#subiz * {all:unset;}')
--- templates/index.js
+++ templates/index.js
@@ -32,7 +32,6 @@ var meta = {
 			title_color: 'black',
 			description_color: 'black',
 			background: 'white',
-			background_image: 'url(' + require('../assets/background/bg1.png') + ')',
 			primary_button_background: '#424E58',
 			primary_button_color: 'white',
 			secondary_button_background: 'transparent',
@@ -64,11 +63,11 @@ var meta = {
 			title_color: 'black',
 			description_color: 'black',
 			background: 'white',
-			background_image: 'url(' + require('../assets/background/bg2.png') + ')',
+			// background_image: 'url(' + require('../assets/background/bg2.png') + ')',
 			primary_button_background: '#FDAD15',
 			primary_button_color: 'white;',
-			secondary_button_background: '',
-			secondary_button_color: '',
+			secondary_button_background: '#777',
+			secondary_button_color: 'white',
 			input_color: '',
 			input_background: '',
 		},
@@ -79,19 +78,30 @@ var meta = {
 				/*webpackPrefetch: true*/ /*webpackChunkName: "template3_css" */ '!to-string-loader!css-loader!less-loader!./template3.less'
 			),
 		type: 'modal',
-		en: {
-			name: 'Sign up offer',
-			title: 'Sign up to get special offer',
-			description: 'Get 20% off your first order. Sign up now!',
-			primary_button: {text: 'Sign up now'},
-			secondary_button: {text: 'Cancel'},
+		text: {
+			name: {vi: 'Form đăng ký nhận ưu đãi', en: 'Sign up offer'},
+			title: {
+				vi: 'Đăng ký để nhận ưu đãi đặc biệt',
+				en: 'Sign up to get special offer',
+			},
+			description: {
+				vi: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
+				en: 'Get 20% off your first order. Sign up now!',
+			},
+			primary_button_text: {vi: 'Đăng ký ngay', en: 'Sign up now'},
+			secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
 		},
-		vi: {
-			name: 'Form đăng ký nhận ưu đãi',
-			title: 'Đăng ký để nhận ưu đãi đặc biệt',
-			description: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
-			primary_button: {text: 'Đăng ký ngay'},
-			secondary_button: {text: 'Bỏ qua'},
+		desktop_appearance: {
+			title_color: 'black',
+			description_color: 'black',
+			background: 'white',
+			// background_image: 'url(' + require('../assets/background/template3.png') + ')',
+			primary_button_background: '#d12',
+			primary_button_color: 'white;',
+			secondary_button_background: '#777',
+			secondary_button_color: 'white',
+			input_color: '',
+			input_background: '',
 		},
 	},
 	template4: {
@@ -100,89 +110,123 @@ var meta = {
 				/*webpackPrefetch: true*/ /*webpackChunkName: "template4_css" */ '!to-string-loader!css-loader!less-loader!./template4.less'
 			),
 		type: 'modal',
-		en: {
-			name: 'Sign up offer',
-			title: 'Sign up to get special offer',
-			description: 'Get 20% off your first order. Sign up now!',
-			primary_button: {text: 'Sign up now'},
-			secondary_button: {text: 'Cancel'},
+		text: {
+			name: {vi: 'Form đăng ký nhận ưu đãi', en: 'Sign up offer'},
+			title: {
+				vi: 'Đăng ký để nhận ưu đãi đặc biệt',
+				en: 'Sign up to get special offer',
+			},
+			description: {
+				vi: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
+				en: 'Get 20% off your first order. Sign up now!',
+			},
+			primary_button_text: {vi: 'Đăng ký ngay', en: 'Sign up now'},
+			secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
 		},
-		vi: {
-			name: 'Form đăng ký nhận ưu đãi',
-			title: 'Đăng ký để nhận ưu đãi đặc biệt',
-			description: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
-			primary_button: {text: 'Đăng ký ngay'},
-			secondary_button: {text: 'Bỏ qua'},
+		desktop_appearance: {
+			title_color: 'black',
+			description_color: 'black',
+			background: 'white',
+			// background_image: 'url(' + require('../assets/background/bg4.png') + ')',
+			primary_button_background: '#001984',
+			primary_button_color: 'white;',
+			secondary_button_background: '#777',
+			secondary_button_color: 'white',
+			input_color: '',
+			input_background: '',
 		},
-		// name: {vi: 'Form đăng ký nhận ưu đãi', en: 'Sign up offer'},
-		// title: {
-		// 	vi: 'Đăng ký nhận ưu đãi khủng',
-		// 	en: 'Sign up to get special offer',
-		// },
-		// description: {
-		// 	vi: 'Giảm giá tới 95%. Miễn phí vận chuyển nội thành',
-		// 	en: 'Get 95% of your first purchase. Free shipping',
-		// },
-		// primary_button_text: {vi: 'Đăng ký ngay', en: 'Sign up now'},
-		// secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
 	},
-	template9: {
-		name: {vi: 'Form hỏi ý kiến (feedback)', en: 'Feedback form'},
-		title: {
-			vi: 'Bạn có tìm được thứ mình cần?',
-			en: 'Did you find what you need?',
+	template5: {
+		css: () =>
+			import(
+				/*webpackPrefetch: true*/ /*webpackChunkName: "template5_css" */ '!to-string-loader!css-loader!less-loader!./template5.less'
+			),
+		type: 'modal',
+		text: {
+			name: {vi: 'Form đăng ký nhận ưu đãi', en: 'Sign up offer'},
+			title: {
+				vi: 'Đăng ký để nhận ưu đãi đặc biệt',
+				en: 'Sign up to get special offer',
+			},
+			description: {
+				vi: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
+				en: 'Get 20% off your first order. Sign up now!',
+			},
+			primary_button_text: {vi: 'Đăng ký ngay', en: 'Sign up now'},
+			secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
 		},
-		description: {
-			vi: 'Chia sẻ cảm nhận của bạn cho chúng tôi bạn sẽ nhận được 10% GIẢM GIÁ.',
-			en: 'Share your recent experience and get 10% off your next purchase',
+		desktop_appearance: {
+			title_color: 'black',
+			description_color: 'black',
+			background: 'white',
+			// background_image: 'url(' + require('../assets/background/bg5.png') + ')',
+			primary_button_background: '#001984',
+			primary_button_color: 'white;',
+			secondary_button_background: '#777',
+			secondary_button_color: 'white',
+			input_color: '',
+			input_background: '',
 		},
-		primary_button_text: {vi: 'Gửi cảm nhận', en: 'Send feedback'},
-		secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
 	},
-	template10: {
-		name: {vi: 'Form hỏi ý kiến (feedback)', en: 'Feedback form'},
-		title: {
-			vi: 'Bạn có tìm được thứ mình cần?',
-			en: 'Did you find what you need?',
-		},
-		description: {
-			vi: 'Chia sẻ cảm nhận của bạn cho chúng tôi bạn sẽ nhận được 10% GIẢM GIÁ.',
-			en: 'Share your recent experience and get 10% off your next purchase',
-		},
-		primary_button_text: {vi: 'Gửi cảm nhận', en: 'Send feedback'},
-		secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
-	},
-	template17: {
-		name: {vi: 'Form đăng ký nhận ưu đãi', en: 'Sign up offer'},
-		title: {
-			vi: 'ĐĂNG KÝ ĐỂ NHẬN ƯU ĐÃI ĐẶC BIỆT',
-			en: 'SIGN UP TO GET SPECIAL OFFER',
-		},
-		description: {
-			vi: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
-			en: 'Get 20% off your first order. Sign up now!',
-		},
-		primary_button_text: {vi: 'Đăng ký ngay', en: 'Sign up now'},
-		secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
-	},
-	templateu23: {
-		name: {vi: 'Pop-up sự kiện', en: 'Event pop-up'},
-		title: {
-			vi: 'CHẶNG ĐƯỜNG MỚI \n THÀNH CÔNG MỚI',
-			en: 'NEW JOURNEY \n NEW SUCCESS',
-		},
-		description: {
-			vi: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
-			en: 'Get 20% off your first order. Sign up now!',
-		},
-		primary_button_text: {vi: 'Đăng ký ngay', en: 'Sign up now'},
-		secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
-	},
+	// template9: {
+	// 	name: {vi: 'Form hỏi ý kiến (feedback)', en: 'Feedback form'},
+	// 	title: {
+	// 		vi: 'Bạn có tìm được thứ mình cần?',
+	// 		en: 'Did you find what you need?',
+	// 	},
+	// 	description: {
+	// 		vi: 'Chia sẻ cảm nhận của bạn cho chúng tôi bạn sẽ nhận được 10% GIẢM GIÁ.',
+	// 		en: 'Share your recent experience and get 10% off your next purchase',
+	// 	},
+	// 	primary_button_text: {vi: 'Gửi cảm nhận', en: 'Send feedback'},
+	// 	secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
+	// },
+	// template10: {
+	// 	name: {vi: 'Form hỏi ý kiến (feedback)', en: 'Feedback form'},
+	// 	title: {
+	// 		vi: 'Bạn có tìm được thứ mình cần?',
+	// 		en: 'Did you find what you need?',
+	// 	},
+	// 	description: {
+	// 		vi: 'Chia sẻ cảm nhận của bạn cho chúng tôi bạn sẽ nhận được 10% GIẢM GIÁ.',
+	// 		en: 'Share your recent experience and get 10% off your next purchase',
+	// 	},
+	// 	primary_button_text: {vi: 'Gửi cảm nhận', en: 'Send feedback'},
+	// 	secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
+	// },
+	// template17: {
+	// 	name: {vi: 'Form đăng ký nhận ưu đãi', en: 'Sign up offer'},
+	// 	title: {
+	// 		vi: 'ĐĂNG KÝ ĐỂ NHẬN ƯU ĐÃI ĐẶC BIỆT',
+	// 		en: 'SIGN UP TO GET SPECIAL OFFER',
+	// 	},
+	// 	description: {
+	// 		vi: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
+	// 		en: 'Get 20% off your first order. Sign up now!',
+	// 	},
+	// 	primary_button_text: {vi: 'Đăng ký ngay', en: 'Sign up now'},
+	// 	secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
+	// },
+	// templateu23: {
+	// 	name: {vi: 'Pop-up sự kiện', en: 'Event pop-up'},
+	// 	title: {
+	// 		vi: 'CHẶNG ĐƯỜNG MỚI \n THÀNH CÔNG MỚI',
+	// 		en: 'NEW JOURNEY \n NEW SUCCESS',
+	// 	},
+	// 	description: {
+	// 		vi: 'Giảm 20% chỉ một ngày duy nhất. Đăng ký ngay để nhận được mã giảm giá!',
+	// 		en: 'Get 20% off your first order. Sign up now!',
+	// 	},
+	// 	primary_button_text: {vi: 'Đăng ký ngay', en: 'Sign up now'},
+	// 	secondary_button_text: {vi: 'Bỏ qua', en: 'Cancel'},
+	// },
 }
 
 // add default js load function for any template dont has custom template
 Object.keys(meta).map(id => {
 	if (meta[id].js) return
+	console.log("	meta.desktop_appearance.background_image", id)
+	meta[id].desktop_appearance.background_image='url(' + require('../assets/background/'+ id +'.png') + ')'
 	meta[id].js = () =>
 		Promise.resolve({
 			default: {
@@ -197,6 +241,7 @@ Object.keys(meta).map(id => {
 									<div class="background"></div>
 									<div class="content">
 										<p class="title">{this.page.title}</p>
+										<div class="title-separator"></div>
 										<p class="description">{this.page.description}</p>
 										{this.form}
 										<div class="buttons">
