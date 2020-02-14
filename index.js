3f895476733fbec9138c540a92db249cc0f7de61
Thanh Pham Kieu <thanhpk@live.com>
2020-02-14 18:07:25 +0700

    fix export

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-import index from './dist/templates/index.js'
-import Template from './dist/templates/template.js'
-
-export default {
-	index,
-	Template,
-}
+import index from './templates/index.js'
+export default index
